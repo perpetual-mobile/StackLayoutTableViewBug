60db51d57b902d1ec460119304045f152180fd86
Falko Schindler <mail@falkoschindler.de>
2014-07-01 11:35:22 +0200

    created example solution reproducing the bug

---
+++ App.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace StackLayoutTableViewBug
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			return new ContentPage { 
+				Content = new StackLayout {
+					Children = {
+						new TableView {
+							Root = new TableRoot {
+								new TableSection {
+									new ViewCell {
+										View = new StackLayout {
+											Children = {
+												new Label {
+													Text = "Blubb",
+												},
+											},
+										},
+									},
+									new ViewCell {
+										View = new StackLayout {
+											Children = {
+												new Label {
+													Text = "Blubb",
+												},
+											},
+										},
+									},
+									new ViewCell {
+										View = new Label {
+											Text = "Bla",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			};
+		}
+	}
+}
+
