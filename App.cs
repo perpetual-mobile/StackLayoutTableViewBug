c0ba2906614500efa08a4a46e7a9565ee4c4ae3d
Falko Schindler <mail@falkoschindler.de>
2014-07-31 15:17:47 +0200

    fixed using current Xamarin.Forms version 1.2.2

--- App.cs
+++ App.cs
@@ -1,48 +1,47 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace StackLayoutTableViewBug
 {
-	public class App
-	{
-		public static Page GetMainPage ()
-		{	
-			return new ContentPage { 
-				Content = new StackLayout {
-					Children = {
-						new TableView {
-							Root = new TableRoot {
-								new TableSection {
-									new ViewCell {
-										View = new StackLayout {
-											Children = {
-												new Label {
-													Text = "Blubb",
-												},
-											},
-										},
-									},
-									new ViewCell {
-										View = new StackLayout {
-											Children = {
-												new Label {
-													Text = "Blubb",
-												},
-											},
-										},
-									},
-									new ViewCell {
-										View = new Label {
-											Text = "Bla",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			};
-		}
-	}
+    public static class App
+    {
+        public static Page GetMainPage()
+        {	
+            return new ContentPage { 
+                Content = new StackLayout {
+                    Children = {
+                        new TableView {
+                            Root = new TableRoot {
+                                new TableSection {
+                                    new ViewCell {
+                                        View = new StackLayout {
+                                            Children = {
+                                                new Label {
+                                                    Text = "Blubb",
+                                                },
+                                            },
+                                        },
+                                    },
+                                    new ViewCell {
+                                        View = new StackLayout {
+                                            Children = {
+                                                new Label {
+                                                    Text = "Blubb",
+                                                },
+                                            },
+                                        },
+                                    },
+                                    new ViewCell {
+                                        View = new Label {
+                                            Text = "Bla",
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            };
+        }
+    }
 }
 
